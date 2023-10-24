8ab5347a3c18d103c2d7f5d6adf54534b01638e0
izharikov <zharikovigor97@email.com>
2023-10-24 17:39:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// Add Express
+const express = require("express");
+const path = require('path');
+
+// Initialize Express
+const app = express();
+
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+app.use(express.static('public'));
+
+app.get("/templates/sample", (req, res) => {
+    res.sendFile(path.join(__dirname, '/templates/sample/index.html'));
+})
+
+// Initialize server
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+
+// Export the Express API
+module.exports = app;
