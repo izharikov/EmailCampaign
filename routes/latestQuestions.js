c6b07ee58adcd6e69c43c520a68ce2add86d111a
izharikov <zharikovigor97@email.com>
2023-10-30 23:50:50 +0100

    add comments + readme

--- routes/latestQuestions.js
+++ routes/latestQuestions.js
@@ -2,8 +2,10 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 
 const DAYS = 7;
 
+// get time 1 week ago
 const fromDate = () => Math.floor((Date.now() - DAYS * 24 * 60 * 60 * 1000) / 1000);
 
+// API url to request latest unanswered questions
 const url = () =>
     'https://api.stackexchange.com/questions/unanswered?order=desc&filter=default&site=sitecore.stackexchange.com&fromdate=' + fromDate();
 
